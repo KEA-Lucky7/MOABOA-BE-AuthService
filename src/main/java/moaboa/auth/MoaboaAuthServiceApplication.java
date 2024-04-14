23264b9c2be69458b9fd2364c4122a7c902c8883
hyunseok <hyun104467@naver.com>
2024-04-14 18:05:23 +0900

    KAN-85 [CHORE]: 인증서버 초기세팅

---
+++ src/main/java/moaboa/auth/MoaboaAuthServiceApplication.java
@@ -0,0 +1,17 @@
+package moaboa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class MoaboaAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaboaAuthServiceApplication.class, args);
+    }
+
+}
