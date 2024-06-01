bb7d2250b8af79863d1b01fb95a8cc2b0ef1ade4
hyunseok <hyun104467@naver.com>
2024-06-01 20:02:55 +0900

    [FIX]: 데이터 정합성으로 인한 command DB 활용
    토큰 관리는 간단한 쿼리이므로 Command DB를 통해 토큰 관리

--- src/main/java/moaboa/auth/member/repository/command/MemberCommandRepository.java
+++ src/main/java/moaboa/auth/member/repository/command/MemberCommandRepository.java
@@ -3,5 +3,9 @@ package moaboa.auth.member.repository.command;
 import moaboa.auth.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberCommandRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findBySocialId(String socialId);
 }
